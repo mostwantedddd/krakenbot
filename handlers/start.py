040d74abd71c5c4792d69b4accf55bfc637970d0
mostwantedddd <127693148+mostwantedddd@users.noreply.github.com>
2026-06-24 10:18:53 -0600

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -20,13 +20,21 @@ def registrar_start(bot):
             reply_markup=markup
         )
 
+    # -------------------------
+    # VER CRÉDITOS
+    # -------------------------
     @bot.message_handler(func=lambda m: m.text == "💰 Mis Créditos")
     def ver_creditos(message):
 
-        creditos = obtener_creditos(message.from_user.id)
+        try:
+            creditos = obtener_creditos(message.from_user.id)
 
-        bot.send_message(
-            message.chat.id,
-            f"💰 Tus créditos disponibles: {creditos}\n\n"
-            "Cada servicio consume 1 crédito."
-        )
+            bot.send_message(
+                message.chat.id,
+                f"💰 Tus créditos disponibles: {creditos}\n\n"
+                "Cada servicio consume 1 crédito."
+            )
+
+        except Exception as e:
+            print("ERROR START:", e)
+            bot.send_message(message.chat.id, "❌ Error al obtener créditos")
